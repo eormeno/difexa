c18433ed0df10aecf916782be8bed7cd37547218
maxireta <maxi_reta@hotmail.com>
2023-08-17 12:23:47 -0300

    agregar-campos-base

---
+++ database/migrations/2023_08_17_151850_add_fields_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('apellido')->nullable();
+            $table->string('nombre')->nullable();
+            $table->string('documento')->nullable();
+            $table->string('temas')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'apellido',
+                'nombre',
+                'documento',
+                'temas',
+            ]);
+        });
+    }
+};
