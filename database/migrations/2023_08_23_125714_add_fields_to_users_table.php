5e43f85ef35f3b058d252975e6790a66720290e9
DAMO\Emilio <eormeno@gmail.com>
2023-08-23 10:01:17 -0300

    agregar-campos-base

---
+++ database/migrations/2023_08_23_125714_add_fields_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('apellido')->after('id');
+            $table->string('nombre')->after('apellido');
+            $table->string('documento')->after('nombre');
+            $table->string('tema')->after('documento');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['apellido', 'nombre', 'documento', 'tema']);
+        });
+    }
+};
