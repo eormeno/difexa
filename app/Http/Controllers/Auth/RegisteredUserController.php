50f64946beac40cd00980b84aedabdbc9ffb883d
Viictor <viictorbalma@gmail.com>
2023-08-23 17:07:14 -0300

    modificar-controlador-registracin

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,13 +31,19 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'apellido' => ['required', 'string', 'max:20', 'min:2'],
+            'nombre' => ['required', 'string', 'max:20', 'min:2'],
+            'documento' => ['required', 'string','max:20'],
+            'tema' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'apellido' => $request->apellido,
+            'nombre' => $request->nombre,
+            'documento' => $request->documento,
+            'tema' => $request->tema,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
