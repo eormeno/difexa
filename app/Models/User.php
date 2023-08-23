d808aec187aebe047f4802d9ecf09cf0c4744954
DAMO\Emilio <eormeno@gmail.com>
2023-08-23 10:27:17 -0300

    modificar-modelo-user

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'apellido',
+        'nombre',
+        'documento',
+        'tema',
         'email',
         'password',
     ];
