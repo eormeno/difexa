4513fe849149cc231df8cdbde90e3433e5723ae0
Emilio Ormeño <eormneno@gmail.com>
2023-08-23 19:21:38 -0300

    crear-modelo-temas-migracin

---
+++ database/migrations/2023_08_23_221307_create_temas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('temas', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 50);
+            $table->string('descripcion', 255);
+            $table->string('slug', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('temas');
+    }
+};
