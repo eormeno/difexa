5c1b884b1d716f988db823992beee78218bf53cd
Juangabrielgonzalez222 <juangabrielgon8@gmail.com>
2023-08-23 19:07:15 -0300

    mostrar-apellido-nombre-vista

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function getFullName(){
+        return ucfirst($this->apellido) . ' ' . ucfirst($this->nombre);
+    }
 }
