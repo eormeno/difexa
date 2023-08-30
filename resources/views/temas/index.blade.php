33761945f39b089e4cc5f0932c5952481c8c98e7
maxireta <maxi_reta@hotmail.com>
2023-08-30 18:16:05 -0300

    crear-vista-estatica-temas

---
+++ resources/views/temas/index.blade.php
@@ -0,0 +1,22 @@
+<x-guest-layout>
+	<div>
+		 <div class="grid grid-cols-4 gap-5">
+			  <div class="rounded-xl bg-gray-300 shadow p-2">
+					<p
+						 class="text-white bg-gray-700 hover:bg-gray-600 cursor-pointer rounded-md text-center p-1 my-2 font-semibold">
+						 {{ 'Aca va el tema en cuestión.' }}
+					</p>
+			  </div>
+			  <div class="rounded-xl bg-gray-300 shadow p-2">
+				  <p
+						class="text-white bg-gray-700 hover:bg-gray-600 cursor-pointer rounded-md text-center p-1 my-2 font-semibold">
+						{{ 'Aca va el tema en cuestión.' }}
+				  </p>
+			 </div>
+		 </div>
+
+		 <div class="my-10">
+			  <p>Paginación</p>
+		 </div>
+	</div>
+</x-guest-layout>
