58390bce3bf7498327466c59f8fc0809484cea39
Viictor <viictorbalma@gmail.com>
2023-08-30 17:49:02 -0300

    crear-vista-estatica-temas

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::get('/temas', function(){
+    return view('temas.index');
+})->name('temas.index');
+
 require __DIR__.'/auth.php';
