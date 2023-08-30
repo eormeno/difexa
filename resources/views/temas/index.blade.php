4e9e025ca6c1e8fb13ce30fdb8c8080b77a5a6da
maxireta <maxi_reta@hotmail.com>
2023-08-30 18:34:13 -0300

    dinamizar-vista-temas-controlador

--- resources/views/temas/index.blade.php
+++ resources/views/temas/index.blade.php
@@ -1,22 +1,25 @@
 <x-guest-layout>
 	<div>
-		 <div class="grid grid-cols-4 gap-5">
-			  <div class="rounded-xl bg-gray-300 shadow p-2">
-					<p
-						 class="text-white bg-gray-700 hover:bg-gray-600 cursor-pointer rounded-md text-center p-1 my-2 font-semibold">
-						 {{ 'Aca va el tema en cuestión.' }}
-					</p>
-			  </div>
-			  <div class="rounded-xl bg-gray-300 shadow p-2">
-				  <p
-						class="text-white bg-gray-700 hover:bg-gray-600 cursor-pointer rounded-md text-center p-1 my-2 font-semibold">
-						{{ 'Aca va el tema en cuestión.' }}
-				  </p>
-			 </div>
-		 </div>
+        <div class="grid grid-cols-4 gap-5">
+            @forelse ($temas as $tema)
+                <div class="rounded-xl bg-gray-300 shadow p-2">
+                    <p
+                        class="text-white bg-gray-700 hover:bg-gray-600 cursor-pointer rounded-md text-center p-1 my-2 font-semibold">
+                        {{ $tema->titulo }}
+                    </p>
+                </div>
+            @empty
+                <div class="rounded-xl bg-gray-300 shadow p-2">
+                    <p
+                        class="text-white bg-gray-700 hover:bg-gray-600 cursor-pointer rounded-md text-center p-1 my-2 font-semibold">
+                        No hay temas
+                    </p>
+                </div>
+            @endforelse
+        </div>
 
-		 <div class="my-10">
-			  <p>Paginación</p>
-		 </div>
-	</div>
+        <div class="my-10">
+            {{ $temas->links() }}
+        </div>
+    </div>
 </x-guest-layout>
