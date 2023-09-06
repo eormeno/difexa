2e0dff9ed321595dbc490ffcc64043890ca0c02f
Juangabrielgonzalez222 <juangabrielgon8@gmail.com>
2023-09-06 19:58:59 -0300

    factory-usuario-admin-inicial

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\TemasSeeder;
 use Database\Seeders\DispositivosTableSeeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'apellido' => 'Admin',
+            'nombre' => 'Admin',
+            'documento' => '00000000',
+            'email' => 'admin@admin',
+            'password'=>Hash::make('12345678'),
+            'is_admin' => true,
+        ]);
         $this->call([TemasSeeder::class]);
         $this->call([DispositivosTableSeeder::class]);
     }
