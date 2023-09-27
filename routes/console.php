df57181cad37559ef1fd2ccb0d1c8f6c88c4de84
Viictor <viictorbalma@gmail.com>
2023-09-27 17:43:19 -0300

    Agregar-nuevos-comandos

--- routes/console.php
+++ routes/console.php
@@ -1,19 +1,21 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Artisan::command('fresh', function () {
+    // if database is sqlite, delete the database file
+    if (config('database.default') === 'sqlite') {
+        if (file_exists(database_path('database.sqlite'))) {
+            unlink(database_path('database.sqlite'));
+        }
+        $this->call('migrate', ['--force' => true]);
+        $this->call('db:seed');
+    }
+});
+
+Artisan::command('users', function () {
+    $users = User::all(['email', 'is_admin', 'is_publisher'])->toArray();
+    $this->table(['email', 'is_admin', 'is_publisher'], $users);
+})->purpose('Display users');
