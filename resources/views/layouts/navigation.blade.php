1147d394583c89ec8b507a02e6e7fd9af58a0f62
joaquinalbornozz <joaquinealbornoz021@gmail.com>
2023-10-04 01:25:28 -0300

    agregar-ruta-dispositivos-navegacion

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,7 +22,13 @@
                         {{ __('Temas') }}
                     </x-nav-link>
                 </div>
-                
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('dispositivos.index')" :active="request()->routeIs('dispositivos.index')">
+                        {{ __('Dispositivos') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
@@ -76,6 +82,18 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Panel') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('temas.index')" :active="request()->routeIs('temas.index')">
+                {{ __('Temas') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('dispositivos.index')" :active="request()->routeIs('dispositivos.index')">
+                {{ __('Dispositivos') }}
             </x-responsive-nav-link>
         </div>
 
