0bb6264d30c395d9f5c60c55a48b24388b89851e
Viictor <viictorbalma@gmail.com>
2023-11-01 12:16:14 -0300

    baja-temas

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
-use App\Http\Controllers\DispositivoController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TemasController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TemasController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\DispositivoController;
 use App\Http\Controllers\PublicacionController;
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -35,30 +34,29 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'is_admin'])->group(function() {
     Route::get('/temas', [TemasController::class, 'index'])->name('temas.index');
-    Route::get('/temas/{id}/edit', [TemasController::class, 'edit'])->name('temas.edit');
-    Route::patch('/temas/{id}', [TemasController::class, 'update'])->name('temas.update');
     Route::get('/temas/create', [TemasController::class, 'create'])->name('temas.create');
     Route::post('/temas', [TemasController::class, 'store'])->name('temas.store');
+    Route::get('/temas/{id}', [TemasController::class, 'edit'])->name('temas.edit');
+    Route::patch('/temas/{id}', [TemasController::class, 'update'])->name('temas.update');
+    Route::delete('/temas/{tema}', [TemasController::class, 'destroy'])->name('temas.destroy');
+
+    Route::get('/usuarios', [UsuariosController::class, 'index'])->name('usuarios.index');
 
     Route::get('/dispositivos', [DispositivoController::class, 'index'])->name('dispositivos.index');
-    Route::get('/dispositivos/{id}', [DispositivoController::class, 'show'])->name('dispositivos.show');
-    Route::get('/dispositivos/{id}', [DispositivoController::class, 'edit'])->name('dispositivos.edit');
-    Route::patch('/dispositivos/{id}', [DispositivoController::class, 'update'])->name('dispositivos.update');
+    Route::get('/dispositivos/create', [DispositivoController::class, 'create'])->name('dispositivos.create');
+    Route::post('/dispositivos', [DispositivoController::class, 'store'])->name('dispositivos.store');
+    Route::get('/dispositivos/{dispositivo}', [DispositivoController::class, 'edit'])->name('dispositivos.edit');
+    Route::patch('/dispositivos/{dispositivo}', [DispositivoController::class, 'update'])->name('dispositivos.update');
     Route::delete('/dispositivos/{dispositivo}', [DispositivoController::class, 'destroy'])->name('dispositivos.destroy');
-    
 });
 
-Route::middleware(['auth','is_publisher'])->group(function () {
+Route::middleware(['auth', 'is_publisher'])->group(function() {
     Route::get('/publicaciones', [PublicacionController::class, 'index'])->name('publicaciones.index');
-    Route::get('/publicaciones/{id}', [PublicacionController::class, 'show'])->name('publicaciones.show');
+    Route::get('/publicaciones/create', [PublicacionController::class, 'create'])->name('publicaciones.create');
+    Route::post('/publicaciones', [PublicacionController::class, 'store'])->name('publicaciones.store');
     Route::get('/publicaciones/{id}', [PublicacionController::class, 'edit'])->name('publicaciones.edit');
     Route::patch('/publicaciones/{id}', [PublicacionController::class, 'update'])->name('publicaciones.update');
+    Route::delete('/publicaciones/{publicacion}', [PublicacionController::class, 'destroy'])->name('publicaciones.destroy');
 });
 
-Route::resource('temas', TemasController::class)->middleware(['auth', 'is_admin'])->name('index', 'temas.index');
-
-Route::resource('dispositivos', DispositivoController::class)->middleware(['auth', 'is_admin'])->name('index', 'dispositivos.index');
-
-Route::resource('publicaciones', PublicacionController::class)->middleware(['auth', 'is_publisher'])->name('index', 'publicaciones.index');
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
